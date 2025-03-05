711f47dbe994a04ad0b694971a4b04f45d226a70
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 22:59:03 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,34 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import usb_host, media_player
+from esphome.components import usb_host
 from esphome.const import CONF_ID, CONF_NAME
 
 CODEOWNERS = ["@your_github_username"]
-DEPENDENCIES = ["usb_host"]  # Changé de media_player à usb_host
-AUTO_LOAD = ["media_player"]  # Mais on charge toujours media_player
+DEPENDENCIES = ["usb_host"]
+AUTO_LOAD = ["usb_host"]
 
 CONF_AUDIO_OUTPUT_MODE = "audio_output_mode"
+
 AUDIO_OUTPUT_MODES = {
     "internal_speakers": 0,
     "usb_headset": 1,
-    "auto_select": 2
+    "auto_select": 2,
 }
 
 usbaudio_ns = cg.esphome_ns.namespace('usbaudio')
-USBAudioComponent = usbaudio_ns.class_('USBAudioComponent', media_player.MediaPlayer, cg.Component)
+USBAudioComponent = usbaudio_ns.class_('USBAudioComponent', cg.Component)
 
-# Création du schéma sous usb_host au lieu de media_player
 CONFIG_SCHEMA = usb_host.USB_HOST_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(USBAudioComponent),
-    cv.Required(CONF_NAME): cv.string,  # Ajout du nom requis
+    cv.Required(CONF_NAME): cv.string,
     cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="auto_select"): cv.enum(AUDIO_OUTPUT_MODES),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await media_player.register_media_player(var, config)  # On l'enregistre toujours comme media_player
-    
-    # Set audio output mode
-    audio_output_mode = config.get(CONF_AUDIO_OUTPUT_MODE)
-    cg.add(var.set_audio_output_mode(AUDIO_OUTPUT_MODES[audio_output_mode]))
+    cg.add(var.set_audio_output_mode(AUDIO_OUTPUT_MODES[config[CONF_AUDIO_OUTPUT_MODE]]))
+
