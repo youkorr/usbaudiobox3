677951c2ee0cf5b902fba1cd5ef85ba9dd76dd2f
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 23:31:16 +0100

    Update usbaudio.cpp

--- components/usbaudio/usbaudio.cpp
+++ components/usbaudio/usbaudio.cpp
@@ -9,40 +9,11 @@ static const char *const TAG = "usbaudio";
 
 void USBAudioComponent::setup() {
   ESP_LOGD(TAG, "Initialisation de l'Audio USB pour ESP32-S3 Box 3");
-  initialize_internal_speakers_();
   initialize_usb_audio_();
 }
 
 void USBAudioComponent::loop() {
   handle_usb_audio_connection_();
-}
-
-void USBAudioComponent::initialize_internal_speakers_() {
-  ESP_LOGD(TAG, "Configuration des haut-parleurs internes via I2S");
-
-  i2s_config_t i2s_config = {
-      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-      .sample_rate = 44100,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-      .dma_buf_count = 8,
-      .dma_buf_len = 64,
-      .use_apll = false,
-      .tx_desc_auto_clear = true,
-      .fixed_mclk = 0
-  };
-
-  i2s_pin_config_t pin_config = {
-      .bck_io_num = GPIO_NUM_41,
-      .ws_io_num = GPIO_NUM_42,
-      .data_out_num = GPIO_NUM_40,
-      .data_in_num = I2S_PIN_NO_CHANGE
-  };
-
-  ESP_ERROR_CHECK(i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL));
-  ESP_ERROR_CHECK(i2s_set_pin(I2S_NUM_0, &pin_config));
 }
 
 void USBAudioComponent::initialize_usb_audio_() {
