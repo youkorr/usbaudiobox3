13e305ae759b5ff2a235a26b1770363e27c55428
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 00:00:38 +0100

    Update usbaudio.cpp

--- components/usbaudio/usbaudio.cpp
+++ components/usbaudio/usbaudio.cpp
@@ -18,8 +18,8 @@ void USBAudioComponent::handle_usb_audio_connection_() {
   bool new_usb_audio_state = detect_usb_audio_device_();
   if (new_usb_audio_state != usb_audio_connected_) {
     usb_audio_connected_ = new_usb_audio_state;
-    if (audio_output_mode_ == AUTO_SELECT) {
-      switch_audio_output_(usb_audio_connected_ ? USB_HEADSET : INTERNAL_SPEAKERS);
+    if (audio_output_mode_ == AudioOutputMode::AUTO_SELECT) {
+      switch_audio_output_(usb_audio_connected_ ? AudioOutputMode::USB_HEADSET : AudioOutputMode::INTERNAL_SPEAKERS);
     }
   }
 }
@@ -30,23 +30,23 @@ bool USBAudioComponent::detect_usb_audio_device_() {
 }
 
 void USBAudioComponent::switch_audio_output_(AudioOutputMode mode) {
-  ESP_LOGD(TAG, "Changement de sortie audio : %d", mode);
+  ESP_LOGD(TAG, "Changement de sortie audio : %d", static_cast<int>(mode));
   switch (mode) {
-    case INTERNAL_SPEAKERS:
+    case AudioOutputMode::INTERNAL_SPEAKERS:
       ESP_LOGD(TAG, "Sortie audio : Haut-parleurs internes");
       break;
-    case USB_HEADSET:
+    case AudioOutputMode::USB_HEADSET:
       ESP_LOGD(TAG, "Sortie audio : Casque USB");
       break;
-    case AUTO_SELECT:
-      switch_audio_output_(usb_audio_connected_ ? USB_HEADSET : INTERNAL_SPEAKERS);
+    case AudioOutputMode::AUTO_SELECT:
+      switch_audio_output_(usb_audio_connected_ ? AudioOutputMode::USB_HEADSET : AudioOutputMode::INTERNAL_SPEAKERS);
       break;
   }
 }
 
 void USBAudioComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Configuration Audio USB:");
-  ESP_LOGCONFIG(TAG, "  Mode audio: %d", this->audio_output_mode_);
+  ESP_LOGCONFIG(TAG, "  Mode audio: %d", static_cast<int>(this->audio_output_mode_));
   ESP_LOGCONFIG(TAG, "  Casque USB connecté: %s", usb_audio_connected_ ? "Oui" : "Non");
 }
 
