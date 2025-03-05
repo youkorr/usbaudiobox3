e002edbdff42071f9bc0ebda1a6e76a52f32112d
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 19:34:18 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,31 +1,34 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import media_player
+from esphome.const import CONF_ID, CONF_NAME
 
 CODEOWNERS = ["@your_github_username"]
 DEPENDENCIES = ["media_player"]
-MULTI_CONF = True
+AUTO_LOAD = ["media_player"]  # Ajoutez ceci pour charger automatiquement le composant media_player
 
 CONF_AUDIO_OUTPUT_MODE = "audio_output_mode"
 AUDIO_OUTPUT_MODES = {
-    "INTERNAL_SPEAKERS": 0,
-    "USB_HEADSET": 1,
-    "AUTO_SELECT": 2
+    "internal_speakers": 0,
+    "usb_headset": 1,
+    "auto_select": 2
 }
 
 usbaudio_ns = cg.esphome_ns.namespace('usbaudio')
 USBAudioComponent = usbaudio_ns.class_('USBAudioComponent', media_player.MediaPlayer, cg.Component)
 
+# Définir le schéma pour la plateforme usbaudio
 CONFIG_SCHEMA = media_player.MEDIA_PLAYER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(USBAudioComponent),
-    cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="AUTO_SELECT"): cv.enum(AUDIO_OUTPUT_MODES, lower=True),
-})
+    cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="auto_select"): cv.enum(AUDIO_OUTPUT_MODES),
+}).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+# Enregistrez la plateforme usbaudio
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield media_player.register_media_player(var, config)
+    await cg.register_component(var, config)
+    await media_player.register_media_player(var, config)
     
     # Set audio output mode
     audio_output_mode = config.get(CONF_AUDIO_OUTPUT_MODE)
-    cg.add(var.set_audio_output_mode(usbaudio_ns.AudioOutputMode[audio_output_mode.upper()]))
+    cg.add(var.set_audio_output_mode(AUDIO_OUTPUT_MODES[audio_output_mode]))
