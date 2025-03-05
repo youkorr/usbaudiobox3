822e74265b7d0bf397f72211e98738287e91870b
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 23:30:40 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,10 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_NAME
-from esphome.components import i2s
 
 DEPENDENCIES = ["esp32"]
-AUTO_LOAD = ["i2s"]
 
 CONF_AUDIO_OUTPUT_MODE = "audio_output_mode"
 
@@ -21,7 +19,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(USBAudioComponent),
     cv.Required(CONF_NAME): cv.string,
     cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="auto_select"): cv.enum(AUDIO_OUTPUT_MODES),
-}).extend(cv.COMPONENT_SCHEMA).extend(i2s.i2s_device_schema())
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -33,8 +31,4 @@ async def to_code(config):
 
     # Add ESP-IDF dependencies
     cg.add_library("esp_private/usb_host", None)
-    cg.add_library("driver/i2s", None)
-
-    # Setup I2S if needed
-    await i2s.setup_i2s_device(var, config)
 
