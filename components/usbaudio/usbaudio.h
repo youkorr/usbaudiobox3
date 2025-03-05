d6c9e06eb590b339f93e686fe6443acc4d9f76ff
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 23:55:32 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -1,21 +1,33 @@
-pragma once
+#pragma once
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esp_vfs_dev.h"
-#include "esp_vfs_usb_serial_jtag.h"
 
 namespace esphome {
 namespace usbaudio {
 
-enum class AudioOutputMode {
+enum AudioOutputMode {
   INTERNAL_SPEAKERS = 0,
   USB_HEADSET = 1,
   AUTO_SELECT = 2
 };
 
 class USBAudioComponent : public Component {
-  // ... (rest of the class definition)
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+
+  void set_audio_output_mode(AudioOutputMode mode) { audio_output_mode_ = mode; }
+  bool is_usb_headset_connected() const { return usb_audio_connected_; }
+
+ protected:
+  void handle_usb_audio_connection_();
+  bool detect_usb_audio_device_();
+  void switch_audio_output_(AudioOutputMode mode);
+
+  AudioOutputMode audio_output_mode_{AUTO_SELECT};
+  bool usb_audio_connected_{false};
 };
 
 }  // namespace usbaudio
