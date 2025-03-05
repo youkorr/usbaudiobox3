c7f743cb4a092d65531d018e7b6c326285dc1c95
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-05 23:55:05 +0100

    Update usbaudio.cpp

--- components/usbaudio/usbaudio.cpp
+++ components/usbaudio/usbaudio.cpp
@@ -8,35 +8,18 @@ static const char *const TAG = "usbaudio";
 
 void USBAudioComponent::setup() {
   ESP_LOGD(TAG, "Initialisation de l'Audio USB pour ESP32-S3 Box 3");
-  initialize_usb_audio_();
 }
 
 void USBAudioComponent::loop() {
   handle_usb_audio_connection_();
 }
 
-void USBAudioComponent::initialize_usb_audio_() {
-  ESP_LOGD(TAG, "Initialisation du mode USB pour l'audio");
-  esp_err_t ret = esp_vfs_dev_usb_serial_jtag_register();
-  if (ret != ESP_OK) {
-    ESP_LOGE(TAG, "Échec de l'initialisation USB: %s", esp_err_to_name(ret));
-  }
-}
-
 void USBAudioComponent::handle_usb_audio_connection_() {
-  if (detect_usb_audio_device_()) {
-    if (!usb_audio_connected_) {
-      usb_audio_connected_ = true;
-      if (audio_output_mode_ == AudioOutputMode::AUTO_SELECT) {
-        switch_audio_output_(AudioOutputMode::USB_HEADSET);
-      }
-    }
-  } else {
-    if (usb_audio_connected_) {
-      usb_audio_connected_ = false;
-      if (audio_output_mode_ == AudioOutputMode::AUTO_SELECT) {
-        switch_audio_output_(AudioOutputMode::INTERNAL_SPEAKERS);
-      }
+  bool new_usb_audio_state = detect_usb_audio_device_();
+  if (new_usb_audio_state != usb_audio_connected_) {
+    usb_audio_connected_ = new_usb_audio_state;
+    if (audio_output_mode_ == AUTO_SELECT) {
+      switch_audio_output_(usb_audio_connected_ ? USB_HEADSET : INTERNAL_SPEAKERS);
     }
   }
 }
@@ -47,47 +30,24 @@ bool USBAudioComponent::detect_usb_audio_device_() {
 }
 
 void USBAudioComponent::switch_audio_output_(AudioOutputMode mode) {
-  ESP_LOGD(TAG, "Changement de sortie audio : %d", static_cast<int>(mode));
+  ESP_LOGD(TAG, "Changement de sortie audio : %d", mode);
   switch (mode) {
-    case AudioOutputMode::INTERNAL_SPEAKERS:
+    case INTERNAL_SPEAKERS:
       ESP_LOGD(TAG, "Sortie audio : Haut-parleurs internes");
       break;
-    case AudioOutputMode::USB_HEADSET:
+    case USB_HEADSET:
       ESP_LOGD(TAG, "Sortie audio : Casque USB");
       break;
-    case AudioOutputMode::AUTO_SELECT:
-      if (usb_audio_connected_) {
-        switch_audio_output_(AudioOutputMode::USB_HEADSET);
-      } else {
-        switch_audio_output_(AudioOutputMode::INTERNAL_SPEAKERS);
-      }
+    case AUTO_SELECT:
+      switch_audio_output_(usb_audio_connected_ ? USB_HEADSET : INTERNAL_SPEAKERS);
       break;
   }
 }
 
-void USBAudioComponent::set_audio_output_mode(AudioOutputMode mode) {
-  this->audio_output_mode_ = mode;
-  switch_audio_output_(mode);
-}
-
-void USBAudioComponent::play() {
-  ESP_LOGD(TAG, "Lecture audio...");
-  // TODO: Implement audio playback
-}
-
-void USBAudioComponent::stop() {
-  ESP_LOGD(TAG, "Arrêt de la lecture audio...");
-  // TODO: Implement audio stop
-}
-
 void USBAudioComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Configuration Audio USB:");
-  ESP_LOGCONFIG(TAG, "  Mode audio: %d", static_cast<int>(this->audio_output_mode_));
+  ESP_LOGCONFIG(TAG, "  Mode audio: %d", this->audio_output_mode_);
   ESP_LOGCONFIG(TAG, "  Casque USB connecté: %s", usb_audio_connected_ ? "Oui" : "Non");
-}
-
-bool USBAudioComponent::is_usb_headset_connected() {
-  return usb_audio_connected_;
 }
 
 }  // namespace usbaudio
