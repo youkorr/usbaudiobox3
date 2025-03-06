d4a9f15a7ace13f948bdfe54f70dbc4d56bb0b5a
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 15:30:34 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,36 +1,26 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 from esphome.components import text_sensor
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ["esp32"]
+DEPENDENCIES = ["text_sensor"]
 
-CONF_AUDIO_OUTPUT_MODE = "audio_output_mode"
-CONF_TEXT_SENSOR = "text_sensor"
+usbaudio_ns = cg.esphome_ns.namespace("usbaudio")
+USBAudioComponent = usbaudio_ns.class_("USBAudioComponent", cg.Component)
 
-AUDIO_OUTPUT_MODES = {
-    "internal_speakers": 0,
-    "usb_headset": 1,
-    "auto_select": 2,
-}
-
-usbaudio_ns = cg.esphome_ns.namespace('usbaudio')
-USBAudioComponent = usbaudio_ns.class_('USBAudioComponent', cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(USBAudioComponent),
-    cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="auto_select"): cv.enum(AUDIO_OUTPUT_MODES),
-    cv.Optional(CONF_TEXT_SENSOR): cv.use_id(text_sensor.TextSensor),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(USBAudioComponent),
+        cv.Optional("text_sensor"): cv.use_id(text_sensor.TextSensor),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    if CONF_AUDIO_OUTPUT_MODE in config:
-        cg.add(var.set_audio_output_mode(AUDIO_OUTPUT_MODES[config[CONF_AUDIO_OUTPUT_MODE]]))
-
-    if CONF_TEXT_SENSOR in config:
-        sens = await cg.get_variable(config[CONF_TEXT_SENSOR])
+    if "text_sensor" in config:
+        sens = await cg.get_variable(config["text_sensor"])
         cg.add(var.set_text_sensor(sens))
+
 
