470e19f038538e39c3c19439f17913deebe2a0e1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 02:04:26 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,10 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.components import text_sensor
 
 DEPENDENCIES = ["esp32"]
 
 CONF_AUDIO_OUTPUT_MODE = "audio_output_mode"
+CONF_TEXT_SENSOR = "text_sensor"
 
 AUDIO_OUTPUT_MODES = {
     "internal_speakers": 0,
@@ -18,6 +20,7 @@ USBAudioComponent = usbaudio_ns.class_('USBAudioComponent', cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(USBAudioComponent),
     cv.Optional(CONF_AUDIO_OUTPUT_MODE, default="auto_select"): cv.enum(AUDIO_OUTPUT_MODES),
+    cv.Optional(CONF_TEXT_SENSOR): cv.use_id(text_sensor.TextSensor),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -26,4 +29,8 @@ async def to_code(config):
     
     if CONF_AUDIO_OUTPUT_MODE in config:
         cg.add(var.set_audio_output_mode(AUDIO_OUTPUT_MODES[config[CONF_AUDIO_OUTPUT_MODE]]))
+
+    if CONF_TEXT_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_TEXT_SENSOR])
+        cg.add(var.set_text_sensor(sens))
 
