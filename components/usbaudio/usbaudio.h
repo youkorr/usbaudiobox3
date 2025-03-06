6af4123d586cc1313b48376122ef92ef57cb30da
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 15:34:00 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -1,16 +1,15 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 
 namespace esphome {
 namespace usbaudio {
 
 enum class AudioOutputMode {
-  INTERNAL_SPEAKERS = 0,
-  BLUETOOTH_OUTPUT = 1,
-  AUTO_SELECT = 2
+  AUTO_SELECT = 0,
+  INTERNAL_SPEAKERS = 1,
+  BLUETOOTH_OUTPUT = 2
 };
 
 class USBAudioComponent : public Component {
@@ -23,30 +22,28 @@ class USBAudioComponent : public Component {
   void set_audio_output_mode(int mode);
   AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
   bool is_bluetooth_connected() const { return bt_connected_; }
+
   void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
 
  protected:
   void apply_audio_output_();
   void update_text_sensor_();
-  
-  // Bluetooth
-  bool detect_bluetooth_dongle_();
   void initialize_bluetooth_a2dp_();
-  void start_bluetooth_pairing_();
-  static void bluetooth_a2dp_callback_(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param);
+  void start_bluetooth_discovery_();
 
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   AudioOutputMode active_output_mode_{AudioOutputMode::INTERNAL_SPEAKERS};
-  
+
   bool bluetooth_dongle_connected_{false};
   bool bt_initialized_{false};
   bool bt_connected_{false};
-  
+
   text_sensor::TextSensor *text_sensor_{nullptr};
 };
-
+  
 }  // namespace usbaudio
 }  // namespace esphome
+
 
 
 
