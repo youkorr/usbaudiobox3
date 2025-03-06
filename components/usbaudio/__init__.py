6f56b6f94d581ea4e57422b14043dce99b5a83c9
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 15:34:22 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -8,10 +8,12 @@ DEPENDENCIES = ["text_sensor"]
 usbaudio_ns = cg.esphome_ns.namespace("usbaudio")
 USBAudioComponent = usbaudio_ns.class_("USBAudioComponent", cg.Component)
 
+CONF_TEXT_SENSOR = "text_sensor"
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(USBAudioComponent),
-        cv.Optional("text_sensor"): cv.use_id(text_sensor.TextSensor),
+        cv.Optional(CONF_TEXT_SENSOR): cv.use_id(text_sensor.TextSensor),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -19,8 +21,9 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    if "text_sensor" in config:
-        sens = await cg.get_variable(config["text_sensor"])
+    if CONF_TEXT_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_TEXT_SENSOR])
         cg.add(var.set_text_sensor(sens))
+
 
 
