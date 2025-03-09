20067aabda7755b67059ec3c602a194882b9dbde
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-09 16:46:19 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -1,17 +1,15 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "usb/usb_host.h"
 
 namespace esphome {
 namespace usbaudio {
 
 enum class AudioOutputMode {
-  INTERNAL_SPEAKERS = 0,
-  USB_HEADSET = 1,
-  AUTO_SELECT = 2
+  AUTO_SELECT = 0,
+  INTERNAL_SPEAKERS = 1,
+  USB_HEADSET = 2,
 };
 
 class USBAudioComponent : public Component {
@@ -19,17 +17,21 @@ class USBAudioComponent : public Component {
   void setup() override;
   void loop() override;
   void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::LATE; }
 
   void set_audio_output_mode(AudioOutputMode mode);
   void set_audio_output_mode(int mode);
-  AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
-  bool is_usb_headset_connected() const { return usb_audio_connected_; }
   void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
+  bool is_usb_audio_connected() const { return usb_audio_connected_; }
+  void update_text_sensor();
+  
+  // Handlers for USB events
+  void handle_device_connection();
+  void handle_device_disconnection();
 
  protected:
-  void apply_audio_output_();
   bool detect_usb_audio_device_();
-  void update_text_sensor();
+  void apply_audio_output_();
 
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   bool usb_audio_connected_{false};
