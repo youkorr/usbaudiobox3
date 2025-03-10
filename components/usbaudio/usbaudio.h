e1cf961e4742f5a68894a444364bb42a8eab4347
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 19:46:22 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -22,8 +22,6 @@ class USBAudioComponent : public Component {
   void set_audio_output_mode(int mode);
   AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
   bool is_usb_headset_connected() const { return usb_audio_connected_; }
-  void set_dminus_pin(GPIOPin *pin) { dminus_pin_ = pin; }
-  void set_dplus_pin(GPIOPin *pin) { dplus_pin_ = pin; }
 
  protected:
   void apply_audio_output_();
@@ -31,8 +29,7 @@ class USBAudioComponent : public Component {
 
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   bool usb_audio_connected_{false};
-  GPIOPin *dminus_pin_{nullptr};
-  GPIOPin *dplus_pin_{nullptr};
+  bool usb_driver_installed_{false};
 };
 
 }  // namespace usbaudio
