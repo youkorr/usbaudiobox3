d4d5eb83443a438ea7af0de6b42bf3f7e096b244
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 17:14:02 +0100

    Update __init__.py

--- components/usbaudio/__init__.py
+++ components/usbaudio/__init__.py
@@ -1,9 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import text_sensor, usb_host
+from esphome.components import text_sensor
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["usb_host"]  # Re-add usb_host dependency
+# No dependencies on usb_host
+DEPENDENCIES = []  
 
 CONF_DP = "dp"  # Broche D+ (USB)
 CONF_DM = "dm"  # Broche D- (USB)
@@ -17,7 +18,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_DP): cv.int_,  # Broche D+ (USB)
     cv.Required(CONF_DM): cv.int_,  # Broche D- (USB)
     cv.Optional(CONF_TEXT_SENSOR): cv.use_id(text_sensor.TextSensor),
-}).extend(cv.COMPONENT_SCHEMA).extend(usb_host.USB_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -26,9 +27,6 @@ async def to_code(config):
     # Configuration des broches GPIO pour USB
     cg.add(var.set_dp_pin(config[CONF_DP]))
     cg.add(var.set_dm_pin(config[CONF_DM]))
-    
-    # Integrate with USB host component
-    await usb_host.setup_usb_host(var, config)
     
     if CONF_TEXT_SENSOR in config:
         sens = await cg.get_variable(config[CONF_TEXT_SENSOR])
