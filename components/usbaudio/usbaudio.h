af5fdb492f751d46fd7a7bcf6f56982a55a7b882
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 17:08:38 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -3,7 +3,6 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "usb/usb_host.h"
 
 namespace esphome {
 namespace usbaudio {
@@ -23,11 +22,8 @@ class USBAudioComponent : public Component {
   void set_audio_output_mode(AudioOutputMode mode);
   void set_audio_output_mode(int mode);
   AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
-  bool is_usb_headset_connected() const { return usb_audio_connected_; }
+  bool is_usb_headset_connected() const { return usb_audio_connected_; }  // Méthode pour le text_sensor
   void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
-
-  void set_dp_pin(int dp_pin) { dp_pin_ = dp_pin; }  // Broche D+ (USB)
-  void set_dm_pin(int dm_pin) { dm_pin_ = dm_pin; }  // Broche D- (USB)
 
  protected:
   void apply_audio_output_();
@@ -37,12 +33,11 @@ class USBAudioComponent : public Component {
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   bool usb_audio_connected_{false};
   text_sensor::TextSensor *text_sensor_{nullptr};
-  int dp_pin_{-1};  // Broche D+ (USB)
-  int dm_pin_{-1};  // Broche D- (USB)
 };
 
 }  // namespace usbaudio
 }  // namespace esphome
+
 
 
 
