a35aaf71764f087d54b8d7e1e0d5ffc8ddf2b13c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 18:13:20 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -23,7 +23,6 @@ class USBAudioComponent : public Component {
   void set_audio_output_mode(int mode);
   AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
   bool is_usb_headset_connected() const { return usb_audio_connected_; }
-  void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
   void set_dminus_pin(GPIOPin *pin) { dminus_pin_ = pin; }
   void set_dplus_pin(GPIOPin *pin) { dplus_pin_ = pin; }
 
@@ -31,17 +30,16 @@ class USBAudioComponent : public Component {
   void handle_usb_audio_connection_();
   void apply_audio_output_();
   bool detect_usb_audio_device_();
-  void update_text_sensor();
 
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   bool usb_audio_connected_{false};
-  text_sensor::TextSensor *text_sensor_{nullptr};
   GPIOPin *dminus_pin_{nullptr};
   GPIOPin *dplus_pin_{nullptr};
 };
 
 }  // namespace usbaudio
 }  // namespace esphome
+
 
 
 
