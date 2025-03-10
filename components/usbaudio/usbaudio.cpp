966939cc536d35b59d2018eedf1b0f1c98ef80be
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-10 18:34:29 +0100

    Update usbaudio.cpp

--- components/usbaudio/usbaudio.cpp
+++ components/usbaudio/usbaudio.cpp
@@ -86,8 +86,8 @@ void USBAudioComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "  Mode: %d", static_cast<int>(audio_output_mode_));
   ESP_LOGCONFIG(TAG, "  Casque USB connecté: %s", usb_audio_connected_ ? "OUI" : "NON");
   if (dminus_pin_ != nullptr && dplus_pin_ != nullptr) {
-    ESP_LOGCONFIG(TAG, "  Broche D-: GPIO%d", dminus_pin_->get_pin());
-    ESP_LOGCONFIG(TAG, "  Broche D+: GPIO%d", dplus_pin_->get_pin());
+    ESP_LOGCONFIG(TAG, "  Broche D-: GPIO%d", dminus_pin_->get_gpio());
+    ESP_LOGCONFIG(TAG, "  Broche D+: GPIO%d", dplus_pin_->get_gpio());
   }
 }
 
