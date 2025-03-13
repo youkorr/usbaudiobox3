05198a0cfdfdacc05b13795453d78cbf88e73a97
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-13 03:39:20 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -3,9 +3,6 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "usb/usb_host.h"
-#include "usb/usb_types_ch9.h"
-#include "usb/usb_types_stack.h"
 
 namespace esphome {
 namespace usbaudio {
@@ -22,31 +19,27 @@ class USBAudioComponent : public Component {
   void loop() override;
   void dump_config() override;
 
-  void set_dminus_pin(GPIOPin *pin) { dminus_pin_ = pin; }
-  void set_dplus_pin(GPIOPin *pin) { dplus_pin_ = pin; }
-  void set_audio_output_mode(AudioOutputMode mode) { audio_output_mode_ = mode; }
-  void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
+  void set_audio_output_mode(AudioOutputMode mode);
+  void set_audio_output_mode(int mode);
+  AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
   bool is_usb_headset_connected() const { return usb_audio_connected_; }
+  void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
 
  protected:
   void handle_usb_audio_connection_();
   void apply_audio_output_();
   bool detect_usb_audio_device_();
   void update_text_sensor();
-  void switch_to_usb_audio();
-  void switch_to_speaker();
-  void configure_gpio();
 
   AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
   bool usb_audio_connected_{false};
-  bool last_state_{false};
   text_sensor::TextSensor *text_sensor_{nullptr};
-  GPIOPin *dminus_pin_{nullptr};
-  GPIOPin *dplus_pin_{nullptr};
 };
 
 }  // namespace usbaudio
 }  // namespace esphome
+
+
 
 
 
