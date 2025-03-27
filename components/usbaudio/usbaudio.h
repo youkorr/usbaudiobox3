8e2fb8f982242c14630dfe5f6b6f067ab2926bb3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-27 19:08:27 +0100

    Update usbaudio.h

--- components/usbaudio/usbaudio.h
+++ components/usbaudio/usbaudio.h
@@ -1,41 +1,62 @@
+usbaudio.h Header File
+
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/media_player/media_player.h"
+
+#ifdef CONFIG_ESP32_S3_USB_OTG
 
 namespace esphome {
 namespace usbaudio {
 
-enum class AudioOutputMode {
-  INTERNAL_SPEAKERS = 0,
-  USB_HEADSET = 1,
-  AUTO_SELECT = 2
+// Enum to represent audio player types
+enum audio_player_t {
+    AUDIO_PLAYER_I2S = 0,
+    AUDIO_PLAYER_USB
 };
 
+// Audio player callback context
+struct audio_player_cb_ctx_t {
+    enum {
+        AUDIO_PLAYER_CALLBACK_EVENT_IDLE = 0,
+        AUDIO_PLAYER_CALLBACK_EVENT_PLAYING,
+        AUDIO_PLAYER_CALLBACK_EVENT_PAUSE
+    } audio_event;
+};
+
+// Mute settings enum
+enum AUDIO_PLAYER_MUTE_SETTING {
+    AUDIO_PLAYER_UNMUTE = 1,
+    AUDIO_PLAYER_MUTE = 0
+};
+
+// Function declarations
+audio_player_t get_audio_player_type(void);
+void *get_audio_player_handle(void);
+uint8_t get_sys_volume(void);
+
+// USB Audio Component
 class USBAudioComponent : public Component {
- public:
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
+public:
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
 
-  void set_audio_output_mode(AudioOutputMode mode);
-  void set_audio_output_mode(int mode);
-  AudioOutputMode get_audio_output_mode() const { return audio_output_mode_; }
-  bool is_usb_headset_connected() const { return usb_audio_connected_; }
-  void set_text_sensor(text_sensor::TextSensor *text_sensor) { text_sensor_ = text_sensor; }
+    // Optional: Media player interface methods
+    void play();
+    void pause();
+    void stop();
+    void set_volume(float volume);
 
- protected:
-  void handle_usb_audio_connection_();
-  void apply_audio_output_();
-  bool detect_usb_audio_device_();
-  void update_text_sensor();
-
-  AudioOutputMode audio_output_mode_{AudioOutputMode::AUTO_SELECT};
-  bool usb_audio_connected_{false};
-  text_sensor::TextSensor *text_sensor_{nullptr};
+private:
+    // Internal state tracking
+    bool is_usb_connected_ = false;
+    float current_volume_ = 1.0;
 };
 
-}  // namespace usbaudio
-}  // namespace esphome
+} // namespace usbaudio
+} // namespace esphome
+
+#endif // CONFIG_ESP32_S3_USB_OTG
 
